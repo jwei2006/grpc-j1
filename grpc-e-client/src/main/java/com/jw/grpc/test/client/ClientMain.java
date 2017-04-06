725d27cc3041b5eebd51b4c5c1595abdcc773a3c
root <jwei2006@163.com>
2017-04-06 18:16:30 +0800

    add log

--- grpc-e-client/src/main/java/com/jw/grpc/test/client/ClientMain.java
+++ grpc-e-client/src/main/java/com/jw/grpc/test/client/ClientMain.java
@@ -1,11 +1,19 @@
 package com.jw.grpc.test.client;
 
+import java.io.FileNotFoundException;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Log4jConfigurer;
 
 public class ClientMain{
 	public static void main(String[] args){
+		try{
+			Log4jConfigurer.initLogging("classpath:config/log4j.xml");
+		}catch(FileNotFoundException e1){
+			e1.printStackTrace();
+		}
 		ApplicationContext context= new ClassPathXmlApplicationContext("config/spring/spring.xml");
 		try{
 //			context.getBean(TestClient.class).sendMessage("asdf", 89);
--- grpc-e-server/src/main/java/com/jw/test/grpc/server/ServerMain.java
+++ grpc-e-server/src/main/java/com/jw/test/grpc/server/ServerMain.java
@@ -1,25 +1,38 @@
 package com.jw.test.grpc.server;
 
+/*import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Log4jConfigurer;
 
 import io.grpc.Server;
-import shj.grpc.server.NettyGrpcServerFactory;
+import shj.grpc.server.NettyGrpcServerFactory;*/
+import shj.grpc.server.manager.ServerManager;
 
 public class ServerMain{
 	public static void main(String[] args){
-		ApplicationContext context= new ClassPathXmlApplicationContext("config/spring/spring.xml");
+		// PropertyConfigurator.configure("config/log4j.xml");
+		// ConfigurationSource
+		/*try{
+			Log4jConfigurer.initLogging("classpath:config/log4j.xml");
+		}catch(FileNotFoundException e1){
+			e1.printStackTrace();
+		}
+		ApplicationContext context = new ClassPathXmlApplicationContext("config/spring/spring.xml");
 		try{
 			Server server = context.getBean("nettyGrpcServerFactory", NettyGrpcServerFactory.class).createServer().start();
-			if (server != null)
-		        server.awaitTermination();
+			if(server != null) server.awaitTermination();
 		}catch(BeansException | IOException e){
 			e.printStackTrace();
 		}catch(InterruptedException e){
 			e.printStackTrace();
-		}
+		}finally{
+			if(null != context) ((ClassPathXmlApplicationContext)context).close();
+		}*/
+//		ServerManager.startServer(null, "config/spring/spring.xml");
+		ServerManager.startServer();
 	}
 }
